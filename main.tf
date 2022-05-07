4ed66e7354c66b13c347e6957b943c2bef263336
Edward Smith <eds@Edwards-MacBook-Air.local>
2022-05-07 22:25:19 +1000

    test

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {}
